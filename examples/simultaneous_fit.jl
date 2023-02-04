a4ad0918c0228a03f7c13b6acbf26bbba159639f
Yuan-Ru Lin <yuanruleonlin@gmail.com>
2023-02-05 04:37:10 +0800

    Add plotting for the example of simultaneous fit

--- examples/simultaneous_fit.jl
+++ examples/simultaneous_fit.jl
@@ -6,7 +6,11 @@ data_comb = [data_pass; data_fail]
 
 model_sig(x) = Normal(x.μ, x.σ)
 model_bkg(x) = Uniform(-5, 5)
-nll_all(x, data) = (x.n_sig + x.n_bkg) - sum(log.(x.n_sig * pdf.(Ref(model_sig(x.sig)), data) + x.n_bkg * pdf.(Ref(model_bkg(x)), data)))
+function nll_all(x, data)
+    (x.n_sig + x.n_bkg) -
+    sum(log.(x.n_sig * pdf.(Ref(model_sig(x.sig)), data) +
+             x.n_bkg * pdf.(Ref(model_bkg(x)), data)))
+end
 
 x0_pass = ComponentVector(sig=(μ=0.0, σ=1.0), n_sig=0.0, n_bkg=100.0)   # n_sig will be rewritten
 x0_fail = ComponentVector(sig=(μ=0.0, σ=1.0), n_sig=0.0, n_bkg=3000.0)  # n_sig will be rewritten
@@ -19,4 +23,19 @@ end
 
 res_comb = optimize(x -> nll_comb(x, data_pass, data_fail), x0_comb)
 
-@show res_comb.minimizer.eff
+@debug begin
+    using Plots
+    @show res_comb.minimizer.eff
+    using HEPAnalysisUtils
+    p_pass = plot(DataHist(), data_pass, -5:1e-1:5,
+        title="Pass (\$n^{\\textrm{pass}}_{\\textrm{sig}}=\\epsilon\\cdot n_{\\textrm{sig}}\$)")
+    p_fail = plot(DataHist(), data_fail, -5:1e-1:5,
+        title="Fail (\$n^{\\textrm{fail}}_{\\textrm{sig}}=(1-\\epsilon)\\cdot n_{\\textrm{sig}}\$)")
+    p_eff_nll = plot(
+        y -> 2optimizeat(x -> nll_comb(x, data_pass, data_fail), res_comb; eff=y).minimum,
+        0.475:0.001:0.525,
+        title="\$-2\\Delta\\log L(\\epsilon)\$", xlabel="\$\\epsilon\$")
+    hline!(p_eff_nll, [1.0])
+    p = plot(p_pass, p_fail, p_eff_nll, layout=(3, 1), size=(600, 1200))
+    display(p)
+end
