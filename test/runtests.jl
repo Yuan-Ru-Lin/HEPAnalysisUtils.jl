e849e830f62ca5a0cea20d1523995785b16f0818
Yuan-Ru Lin <yuanruleonlin@gmail.com>
2023-02-07 00:02:58 +0800

    Remove a test that is actually an example

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,5 @@
 using Test
 using HEPAnalysisUtils
-using Optim
-using Distributions
-
-data = randn(100)
-res = optimize([-Inf, 0.0], [Inf, Inf], [0.0, 1.0], Fminbox(BFGS()), Optim.Options(extended_trace=true, callback=miunitstop)) do pars
-    -loglikelihood(Normal(pars...), data)
-end
 
 @test nNeutralB.val ≈ 3.74988366e8
 
